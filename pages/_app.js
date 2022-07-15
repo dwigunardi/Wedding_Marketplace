5189ec11cb6794be74602db0a8d0608a2d5de5f9
unknown <dwigunardi@gmail.com>
2022-07-15 14:38:51 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,19 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  // (
+
+  //   <>
+  //     <head>
+  //       
+  //     </head>
+  //     <body>
+
+  //     </body>
+
+  //   </>
+
+  // )
 }
 
 export default MyApp
---
+++ pages/admin/dashboard.js
@@ -0,0 +1,12 @@
+import NavbarAdmin from "./layoutHeader"
+
+
+function Dashboard() {
+
+    return (
+        <>
+            <NavbarAdmin />
+        </>
+    )
+}
+export default Dashboard
